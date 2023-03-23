34b70b1056fbc9086dd6a067632767f3a47b6b02
yousseffekry <mr-youssefsherif@hotmail.com>
2023-03-23 17:31:31 +0200

    created an express app to retrieve data from the resource

--- express/server.js
+++ express/server.js
@@ -9,6 +9,9 @@
 // import the express module
 const express = require('express');
 
+// import the array from resources
+const {users} = require('./resources/users');
+
 // create express application by using the express function
 // exported by the express module
 // it is called 'app' by convention
@@ -23,15 +26,19 @@ let PORT = 80;
  * handler is the callback function to be executed
  */
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send(users);
 })
 
 /**
  * route parameters are used to get the corresponding values in the URL segment
  * the key-value pairs are published in the req.params object
  */
-app.get('/api/:userId/courses/:courseId', (req, res) => {
-    res.send(req.params);
+app.get('/users/:userId', (req, res) => {
+    const userData = users.find(u => parseInt(u.userID) === parseInt(req.params.userId));
+    if (userData)
+        res.status(200).send(userData);
+    else
+        res.status(404).send('Given id not found');
 })
 
 app.listen(PORT, (err) => {
